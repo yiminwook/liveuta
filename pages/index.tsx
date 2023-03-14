5000ee6adbad0ad3583837a650b2c9aab44b36ed
yiminwook <grs0416@gmail.com>
2023-03-14 19:25:04 +0900

    Fix: main div

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,80 +43,73 @@ const Home: NextPage<Props> = () => {
   return (
     <>
       <ServiceLayout title="LiveUta Home">
-        <main className={home.main}>
-          {isLoading ? (
-            <div className="loading">
-              <div className="loading__container">
-                <div className="loading_center">
-                  <Image
-                    src="/utawaku.png"
-                    width={100}
-                    height={100}
-                    alt="loading_img"
-                  ></Image>
-                  <div>Loading Now</div>
-                </div>
+        {isLoading ? (
+          <div className="loading">
+            <div className="loading__container">
+              <div className="loading_center" style={{ border: "none" }}>
+                <Image
+                  src="/utawaku.png"
+                  width={100}
+                  height={100}
+                  alt="loading_img"
+                ></Image>
+                <div>Loading Now</div>
               </div>
             </div>
-          ) : (
-            <>
-              <div className={home.total}>
-                <div>
-                  {total ? `Total: ${total}` : "검색된 결과가 없습니다."}
-                </div>
-              </div>
-              <div className={home.contents}>
-                {contents.length > 0 &&
-                  contents.map((data) => {
-                    return (
-                      <div
-                        className={home.youtube__container}
-                        key={data.videoId}
-                      >
-                        <div className={home.youtube_content__container}>
-                          <div className={home.youtube_thumnail__container}>
-                            <Link href={data.url ?? ""}>
-                              <Image
-                                src={data.thumbnailUrl}
-                                width={480}
-                                height={360}
-                                alt="thumbnail"
-                                loading="lazy"
-                                placeholder="blur"
-                                blurDataURL="/thumbnail_alt_img.jpg"
-                              ></Image>
-                            </Link>
+          </div>
+        ) : (
+          <main className={home.main}>
+            <div className={home.total}>
+              <div>{total ? `Total: ${total}` : "검색된 결과가 없습니다."}</div>
+            </div>
+            <div className={home.contents}>
+              {contents.length > 0 &&
+                contents.map((data) => {
+                  return (
+                    <div className={home.youtube__container} key={data.videoId}>
+                      <div className={home.youtube_content__container}>
+                        <div className={home.youtube_thumnail__container}>
+                          <Link href={data.url ?? ""}>
+                            <Image
+                              src={data.thumbnailUrl}
+                              width={480}
+                              height={360}
+                              alt="thumbnail"
+                              loading="lazy"
+                              placeholder="blur"
+                              blurDataURL="/thumbnail_alt_img.jpg"
+                            ></Image>
+                          </Link>
+                        </div>
+                        <div className={home.youtube_description}>
+                          <div className={home.youtube_channel_name}>
+                            {data.channelName ?? "no channel name"}
                           </div>
-                          <div className={home.youtube_description}>
-                            <div className={home.youtube_channel_name}>
-                              {data.channelName ?? "no channel name"}
+                          <div className={home.youtube_title}>
+                            {data.title ?? "no title"}
+                          </div>
+                          <Link
+                            className={home.youtube_link}
+                            href={data.url ?? ""}
+                          >
+                            {data.url ?? "no url"}
+                          </Link>
+                          <div className={home.youtube_time__container}>
+                            <div className={home.youtube_time__kor}>
+                              {data.korTime ?? "no sheduled time"}
                             </div>
-                            <div className={home.youtube_title}>
-                              {data.title ?? "no title"}
-                            </div>
-                            <Link
-                              className={home.youtube_link}
-                              href={data.url ?? ""}
-                            >
-                              {data.url ?? "no url"}
-                            </Link>
-                            <div className={home.youtube_time__container}>
-                              <div className={home.youtube_time__kor}>
-                                {data.korTime ?? "no sheduled time"}
-                              </div>
-                              <div className={home.youtube_time__inter}>
-                                {data.iterval ? `(${data.iterval})` : ""}
-                              </div>
+                            <div className={home.youtube_time__inter}>
+                              {data.iterval ? `(${data.iterval})` : ""}
                             </div>
                           </div>
                         </div>
                       </div>
-                    );
-                  })}
-              </div>
-            </>
-          )}
-        </main>
+                    </div>
+                  );
+                })}
+            </div>
+          </main>
+        )}
       </ServiceLayout>
     </>
   );
