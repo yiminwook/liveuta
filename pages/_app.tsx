5eec82807bb3b9e64b1af17253be17130127cbaf
yiminwook <grs0416@gmail.com>
2023-03-14 19:06:54 +0900

    Add: background, update mixin, map scss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { Analytics } from "@vercel/analytics/react";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <div className="background"></div>
       <Component {...pageProps} />
       <Analytics />
     </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import home from "@/styles/Home.module.scss";
-import { NextPage, GetServerSideProps } from "next";
+import { NextPage } from "next";
 import axios, { AxiosResponse } from "axios";
-import getBaseUrl from "@/utils/get_base_url";
 import ServiceLayout from "../components/service_layout";
 import Link from "next/link";
 import { UpcomingData } from "../models/sheet/in_sheet";
 import Image from "next/image";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 interface Props {
@@ -15,7 +14,7 @@ interface Props {
 }
 
 const Home: NextPage<Props> = () => {
-  const router = useRouter();
+  // const router = useRouter();
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [total, setTotal] = useState<number>(0);
   const [contents, setContents] = useState<UpcomingData[]>([]);
@@ -30,7 +29,6 @@ const Home: NextPage<Props> = () => {
       const result: AxiosResponse<{ total: number; upcoming: UpcomingData[] }> =
         await axios.get("/api/sheet/upcoming");
       if (result.status === 200 && result.data.total > 0) {
-        console.log(result);
         const { total, upcoming } = result.data;
         setTotal((_pre) => total);
         setContents((_pre) => [...upcoming]);
@@ -47,71 +45,75 @@ const Home: NextPage<Props> = () => {
       <ServiceLayout title="LiveUta Home">
         <main className={home.main}>
           {isLoading ? (
-            <div
-              style={{
-                position: "fixed",
-                top: "0",
-                left: "0",
-                fontSize: "2rem",
-                backgroundColor: "rgba(0,0,0,0.5)",
-                width: "100vw",
-                height: "100vh",
-                textAlign: "center",
-                verticalAlign: "middle",
-                lineHeight: "100vh",
-              }}
-            >
-              Loading...
+            <div className="loading">
+              <div className="loading__container">
+                <div className="loading_center">
+                  <Image
+                    src="/utawaku.png"
+                    width={100}
+                    height={100}
+                    alt="loading_img"
+                  ></Image>
+                  <div>Loading Now</div>
+                </div>
+              </div>
             </div>
           ) : (
             <>
               <div className={home.total}>
-                {total ? `Total: ${total}` : "검색된 결과가 없습니다."}
+                <div>
+                  {total ? `Total: ${total}` : "검색된 결과가 없습니다."}
+                </div>
               </div>
-              {contents.length > 0 &&
-                contents.map((data) => {
-                  return (
-                    <div className={home.youtube__container} key={data.videoId}>
-                      <div className={home.youtube_content__container}>
-                        <div className={home.youtube_thumnail__container}>
-                          <Link href={data.url ?? ""}>
-                            <Image
-                              src={data.thumbnailUrl}
-                              width={480}
-                              height={360}
-                              alt="thumbnail"
-                              loading="lazy"
-                              placeholder="blur"
-                              blurDataURL="/thumbnail_alt_img.jpg"
-                            ></Image>
-                          </Link>
-                        </div>
-                        <div className={home.youtube_description}>
-                          <div className={home.youtube_channel_name}>
-                            {data.channelName ?? "no channel name"}
+              <div className={home.contents}>
+                {contents.length > 0 &&
+                  contents.map((data) => {
+                    return (
+                      <div
+                        className={home.youtube__container}
+                        key={data.videoId}
+                      >
+                        <div className={home.youtube_content__container}>
+                          <div className={home.youtube_thumnail__container}>
+                            <Link href={data.url ?? ""}>
+                              <Image
+                                src={data.thumbnailUrl}
+                                width={480}
+                                height={360}
+                                alt="thumbnail"
+                                loading="lazy"
+                                placeholder="blur"
+                                blurDataURL="/thumbnail_alt_img.jpg"
+                              ></Image>
+                            </Link>
                           </div>
-                          <div className={home.youtube_title}>
-                            {data.title ?? "no title"}
-                          </div>
-                          <Link
-                            className={home.youtube_link}
-                            href={data.url ?? ""}
-                          >
-                            {data.url ?? "no url"}
-                          </Link>
-                          <div className={home.youtube_time__container}>
-                            <div className={home.youtube_time__kor}>
-                              {data.korTime ?? "no sheduled time"}
+                          <div className={home.youtube_description}>
+                            <div className={home.youtube_channel_name}>
+                              {data.channelName ?? "no channel name"}
                             </div>
-                            <div className={home.youtube_time__inter}>
-                              {data.iterval ? `(${data.iterval})` : ""}
+                            <div className={home.youtube_title}>
+                              {data.title ?? "no title"}
+                            </div>
+                            <Link
+                              className={home.youtube_link}
+                              href={data.url ?? ""}
+                            >
+                              {data.url ?? "no url"}
+                            </Link>
+                            <div className={home.youtube_time__container}>
+                              <div className={home.youtube_time__kor}>
+                                {data.korTime ?? "no sheduled time"}
+                              </div>
+                              <div className={home.youtube_time__inter}>
+                                {data.iterval ? `(${data.iterval})` : ""}
+                              </div>
                             </div>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+              </div>
             </>
           )}
         </main>
