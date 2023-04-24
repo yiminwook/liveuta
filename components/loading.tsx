537eb7d0011d66bb1826605128a857dc7ed3742e
yiminwook <grs0416@gmail.com>
2023-04-25 02:49:22 +0900

    Delete: loading.png blur

--- components/loading.tsx
+++ components/loading.tsx
@@ -1,22 +1,13 @@
 import Image from 'next/image';
 import loading from '@/styles/loading.module.scss';
 import { AiOutlineLoading } from 'react-icons/ai';
-import loadingImage from '/public/loading.png';
 
 const Loading = () => {
   return (
     <div className={loading['loading']}>
       <div>
         <div>
-          <Image
-            src={loadingImage}
-            width={100}
-            height={100}
-            alt="loading_img"
-            placeholder="blur"
-            unoptimized
-            priority
-          />
+          <Image src="/loading.png" width={100} height={100} alt="loading_img" unoptimized priority />
           <AiOutlineLoading size={130} color={'inherit'} />
         </div>
         <h2>Loading Now!</h2>
