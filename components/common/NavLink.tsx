d8b584cd0f2eb911186d1c2cd5f2181bf5c209e1
yiminwook <grs0416@gmail.com>
2023-04-26 03:12:36 +0900

    Add: navTab

--- components/common/NavLink.tsx
+++ components/common/NavLink.tsx
@@ -1,18 +1,28 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { ReactNode } from 'react';
+import { MouseEvent, ReactNode } from 'react';
 
 interface NavLinkProps {
   href: string;
   children: ReactNode;
+  modifier?: string;
 }
-const NavLink = ({ href, children }: NavLinkProps) => {
+const NavLink = ({ href, children, modifier = '' }: NavLinkProps) => {
   const router = useRouter();
   const isActive = router.asPath === href;
 
+  const onClick = (e: MouseEvent) => {
+    if (isActive) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+  };
+
   return (
-    <li className={isActive ? 'active' : ''}>
-      <Link href={href}>{children}</Link>
+    <li className={isActive ? ['active', modifier].join(' ') : ''}>
+      <Link href={href} onClick={onClick}>
+        {children}
+      </Link>
     </li>
   );
 };
