c3f89e8b90077be2e7d455e9a1892910f0d32a2f
pekochan069 <pekochan069@gmail.com>
2023-05-07 16:01:57 +0900

    Fix: allow clipboard write

--- components/common/Iframe.tsx
+++ components/common/Iframe.tsx
@@ -9,7 +9,7 @@ const Iframe = ({ url }: IframeProps) => {
   return (
     <section className={iframe['iframe']}>
       <div>
-        <iframe id="liveuta-iframe" src={url} scrolling="no" />
+        <iframe id="liveuta-iframe" src={url} scrolling="no" allow="clipboard-write" />
       </div>
       <button onClick={() => openWindow(url)}>새 탭에서 열기</button>
     </section>
