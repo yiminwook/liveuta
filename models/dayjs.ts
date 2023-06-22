11a9e551b73a85d062368bee842daad3e50142d0
yiminwook <grs0416@gmail.com>
2023-06-22 22:33:45 +0900

    Fix: dayjs timeZone

--- models/dayjs.ts
+++ models/dayjs.ts
@@ -1,12 +1,11 @@
-import base from 'dayjs';
+import dayjs from 'dayjs';
 import timezone from 'dayjs/plugin/timezone';
 import utc from 'dayjs/plugin/utc';
 import ko from 'dayjs/locale/ko';
 
-const dayjs = base;
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.tz.setDefault('Asia/Seoul');
 dayjs.locale(ko);
 
-export default dayjs;
+export default dayjs.tz;
