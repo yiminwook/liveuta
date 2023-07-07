66e8f470a0a41154d570c8a684874e5a1155f425
yiminwook <grs0416@gmail.com>
2023-07-08 00:11:13 +0900

    Feat: migration short request page

---
+++ app/short/page.tsx
@@ -0,0 +1,20 @@
+import Iframe from '@/components/common/Iframe';
+import { serverEnvConfig } from '@/configs';
+
+const getShortUrl = async () => {
+  const { SHORT_URL } = serverEnvConfig();
+
+  return { url: SHORT_URL };
+};
+
+const ShortPage = async () => {
+  const { url } = await getShortUrl();
+
+  return (
+    <>
+      <Iframe url={url} />
+    </>
+  );
+};
+
+export default ShortPage;
