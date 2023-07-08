343e8c10ef2a85664dea60a0a825e6dadc6719c7
yiminwook <grs0416@gmail.com>
2023-07-09 01:21:34 +0900

    Fix: external link

--- components/layout/sidebar/ExternalLinksSection.tsx
+++ components/layout/sidebar/ExternalLinksSection.tsx
@@ -1,5 +1,13 @@
 import { RxLink2 } from 'react-icons/rx';
-import NavLink from '@/components/common/NavLink';
+import { ReactNode } from 'react';
+
+const ExternalLink = ({ href, text }: { href: string; text: ReactNode }) => {
+  return (
+    <li>
+      <a href={href}>{text}</a>
+    </li>
+  );
+};
 
 const ExternalLinksSection = () => {
   return (
@@ -9,9 +17,8 @@ const ExternalLinksSection = () => {
         &nbsp;외부링크
       </h2>
       <ul>
-        <NavLink href="https://gall.dcinside.com/mgallery/board/lists?id=kizunaai">키즈나아이 갤러리</NavLink>
-        <NavLink href="https://gall.dcinside.com/mini/board/lists?id=vuta">우타와꾸 갤러리</NavLink>
-        <NavLink href="https://www.piku.co.kr/w/6js7eW">아이도루 월드컵</NavLink>
+        <ExternalLink href="https://gall.dcinside.com/mgallery/board/lists?id=kizunaai" text="키즈나아이 갤러리" />
+        <ExternalLink href="https://gall.dcinside.com/mini/board/lists?id=vuta" text="우타와꾸 갤러리" />
       </ul>
     </section>
   );
