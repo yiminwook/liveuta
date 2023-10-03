081233942dc6766c093d1b044f91ba7281b34e10
yiminwook <grs0416@gmail.com>
2023-10-03 15:11:41 +0900

    Feat: select시 mutate, router 초기화

--- components/home/NavSection.tsx
+++ components/home/NavSection.tsx
@@ -1,11 +1,13 @@
 import home from '@/components/home/Home.module.scss';
 import NavLink from '@/components/common/NavLink';
-import { ReactNode } from 'react';
+import React, { ReactNode, useEffect, useMemo, useState } from 'react';
 import HorizonScrollBox from '@/components/common/HorizonScrollBox';
-
-interface NavSectionProps {
-  total: number;
-}
+import useSheet from '@/hooks/api/useSheet';
+import { usePathname, useRouter } from 'next/navigation';
+import Cookies from 'universal-cookie';
+import { SheetAPIReturntype } from '@/types/inSheet';
+import { combineClassName } from '@/utils/combineClassName';
+import { BiArrowFromLeft } from 'react-icons/bi';
 
 const NavTapLink = ({ href, text }: { href: string; text: ReactNode }) => {
   return (
@@ -26,12 +28,80 @@ const NavTap = () => {
   );
 };
 
-const NavSection = ({ total }: NavSectionProps) => {
+const NavSelectBox = ({ select }: { select: string }) => {
+  const [active, setActive] = useState(false);
+  const filter = (usePathname()?.split('/')[1] || 'scheduled') as keyof SheetAPIReturntype;
+  const router = useRouter();
+
+  const { mutate, data } = useSheet();
+
+  const handleSelect = (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
+    const target = e.target as HTMLElement;
+    const li = target.closest('li')?.dataset.value;
+    if (!li || li === select) return;
+    const selectCookie = new Cookies();
+    selectCookie.set('select', li, { path: '/', maxAge: 60 * 60 * 24 * 30 * 3 }); //3개월 저장
+    mutate();
+    router.refresh(); //select 최신화
+  };
+
+  const totalText = `전체: ${data?.[filter].length.total}`;
+  const streamText = `실시간: ${data?.[filter].length.stream}`;
+  const videoText = `동영상: ${data?.[filter].length.video}`;
+
+  const selectedText = useMemo(() => {
+    switch (select) {
+      case 'stream':
+        return streamText;
+      case 'video':
+        return videoText;
+      default:
+        return totalText;
+    }
+  }, [select]);
+
+  const handleToggle = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setActive((pre) => !pre);
+  };
+
+  const handleClose = (e: MouseEvent) => {
+    e.stopPropagation();
+    setActive(() => false);
+  };
+
+  useEffect(() => {
+    window.addEventListener('click', handleClose);
+    return () => window.removeEventListener('click', handleClose);
+  }, []);
+
+  return (
+    <div className={home['nav-selectbox']}>
+      <button onClick={handleToggle}>{selectedText}</button>
+      <div className={combineClassName(home['side'], active ? home['active'] : '')}>
+        <button>
+          <BiArrowFromLeft width="2rem" />
+        </button>
+        <ul onClick={handleSelect}>
+          <li data-value={'all'}>{totalText}</li>
+          <li data-value={'stream'}>{streamText}</li>
+          <li data-value={'video'}>{videoText}</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+interface NavSectionProps {
+  select: string;
+}
+
+const NavSection = ({ select }: NavSectionProps) => {
   return (
     <section className={home['nav-section']}>
       <HorizonScrollBox className={home['nav-scroll-box']}>
         <NavTap />
-        {total > 0 ? <div className={home['total']}>{`Total: ${total}`}</div> : null}
+        <NavSelectBox select={select} />
       </HorizonScrollBox>
     </section>
   );
