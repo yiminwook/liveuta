80bc0cd6d06dc4b6032b2d42721ce30e3bf5a8f6
yiminwook <grs0416@gmail.com>
2023-11-17 23:30:25 +0900

    Fix: Settings render 함수

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -8,18 +8,18 @@ const SettingsPage = () => {
   const token = useAtomValue(tokenAtom);
 
   const renderTokenBox = () => {
-    switch (typeof token) {
-      case 'string':
+    switch (token) {
+      case null:
+        return <div>토큰을 가져오는 중입니다.</div>;
+      case undefined:
+        return <div>토큰을 가져오는데 실패했습니다.</div>;
+      default:
         return (
           <>
             <div id={Settings['token']}>{token}</div>
             <CopyButton value={token!} size={'1.5rem'} />
           </>
         );
-      case 'undefined':
-        return <div>토큰을 가져오는데 실패했습니다.</div>;
-      default:
-        return <div>토큰을 가져오는 중입니다.</div>;
     }
   };
 
--- configs/ServiceWorker.tsx
+++ configs/ServiceWorker.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { tokenAtom } from '@/atoms';
 import FirebaseClient from '@/models/firebase/client';
-import { MessagePayload, getToken, onMessage } from 'firebase/messaging';
+import { MessagePayload, getToken, onMessage, Messaging } from 'firebase/messaging';
 import { useSetAtom } from 'jotai';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
@@ -9,7 +9,25 @@ import { toast } from 'react-toastify';
 const ServiceWorker = () => {
   const setTokenAtom = useSetAtom(tokenAtom);
 
-  const handleToken = async () => {};
+  const handleToken = async ({
+    register,
+    messaging,
+  }: {
+    register: ServiceWorkerRegistration;
+    messaging: Messaging;
+  }) => {
+    try {
+      const token = await getToken(messaging, {
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+        serviceWorkerRegistration: register,
+      });
+
+      setTokenAtom(() => token);
+    } catch (error) {
+      console.error(error);
+      setTokenAtom(() => undefined);
+    }
+  };
 
   const handleMessage = async () => {
     try {
@@ -18,19 +36,10 @@ const ServiceWorker = () => {
       const permission = await Notification.requestPermission();
       if (permission !== 'granted') return;
       //알림허용설정을 거절하면 사용자가 직접 크롬에서 설정값을 변경해야함
+
       const messaging = FirebaseClient.getInstance().message;
 
-      try {
-        const token = await getToken(messaging, {
-          vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
-          serviceWorkerRegistration: register,
-        });
-
-        setTokenAtom(() => token);
-      } catch (error) {
-        console.error(error);
-        setTokenAtom(() => undefined);
-      }
+      await handleToken({ register, messaging });
 
       //TODO: 토큰을 서버로 전달
       onMessage(messaging, ({ notification, data, fcmOptions, from, collapseKey, messageId }: MessagePayload) => {
