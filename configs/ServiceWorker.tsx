c367caf4dcb95293670b25bb73db154b6f1ed002
yiminwook <grs0416@gmail.com>
2023-11-17 23:23:14 +0900

    TEST: token 에러핸들링 수정

--- configs/ServiceWorker.tsx
+++ configs/ServiceWorker.tsx
@@ -9,6 +9,8 @@ import { toast } from 'react-toastify';
 const ServiceWorker = () => {
   const setTokenAtom = useSetAtom(tokenAtom);
 
+  const handleToken = async () => {};
+
   const handleMessage = async () => {
     try {
       //가장 먼저 서비스워커를 등록해야함.
@@ -18,12 +20,17 @@ const ServiceWorker = () => {
       //알림허용설정을 거절하면 사용자가 직접 크롬에서 설정값을 변경해야함
       const messaging = FirebaseClient.getInstance().message;
 
-      const token = await getToken(messaging, {
-        vapidKey: undefined, //ios에서 vapidKey가 있으면 token이 발급되지 않음.
-        serviceWorkerRegistration: register,
-      });
+      try {
+        const token = await getToken(messaging, {
+          vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+          serviceWorkerRegistration: register,
+        });
 
-      setTokenAtom(() => token);
+        setTokenAtom(() => token);
+      } catch (error) {
+        console.error(error);
+        setTokenAtom(() => undefined);
+      }
 
       //TODO: 토큰을 서버로 전달
       onMessage(messaging, ({ notification, data, fcmOptions, from, collapseKey, messageId }: MessagePayload) => {
@@ -35,7 +42,6 @@ const ServiceWorker = () => {
       });
     } catch (error) {
       console.error(error);
-      setTokenAtom(() => undefined);
     }
   };
 
