e6545b18b5406569db18d5bbb891f2fb8ede1964
yiminwook <grs0416@gmail.com>
2023-11-18 09:12:51 +0900

    Add: 알림허용 재요청 기능추가

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,11 +1,16 @@
 'use client';
 import { useAtomValue } from 'jotai';
-import { tokenAtom } from '@/atoms';
+import { tokenAtom, useTokenAtom } from '@/atoms';
 import Settings from '@/components/settings/Settings.module.scss';
 import CopyButton from '@/components/common/button/CopyButton';
+import { toast } from 'react-toastify';
+import FirebaseClient from '@/models/firebase/client';
+import { getToken } from 'firebase/messaging';
+import { useEffect, useState } from 'react';
 
 const SettingsPage = () => {
-  const token = useAtomValue(tokenAtom);
+  const [token, setToken] = useTokenAtom();
+  const [permission, setPermission] = useState('설정을 가져오는 중');
 
   const renderTokenBox = () => {
     switch (token) {
@@ -23,8 +28,45 @@ const SettingsPage = () => {
     }
   };
 
+  const requerstPermission = async () => {
+    try {
+      const permission = await Notification.requestPermission();
+
+      if (permission !== 'granted') {
+        throw new Error('브라우저 알림허용 설정이 실패하였습니다.');
+      }
+
+      //허용후 등록
+      const register = await navigator.serviceWorker.register('/sw.js');
+      const messaging = FirebaseClient.getInstance().message;
+      const token = await getToken(messaging, {
+        vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+        serviceWorkerRegistration: register,
+      });
+      setPermission(() => permission);
+      setToken(() => token);
+      toast.success('알림허용 설정이 완료되었습니다.');
+    } catch (error) {
+      console.error(error);
+      const message = error instanceof Error ? error.message : 'Unknown Error';
+      setPermission(() => 'denied');
+      setToken(() => undefined);
+      toast.error(message);
+    }
+  };
+
+  useEffect(() => {
+    setPermission(() => Notification.permission);
+  }, []);
+
   return (
     <section className={Settings['section']}>
+      <div className={Settings['permission-box']}>
+        <div>
+          알림허용설정 여부: <b>{permission}</b>
+        </div>
+        <button onClick={requerstPermission}>활성요청</button>
+      </div>
       <div className={Settings['token-box']}>
         <label htmlFor="token">SW Token</label>
         <div>{renderTokenBox()}</div>
--- configs/ServiceWorker.tsx
+++ configs/ServiceWorker.tsx
@@ -7,7 +7,7 @@ import { useEffect } from 'react';
 import { toast } from 'react-toastify';
 
 const ServiceWorker = () => {
-  const setTokenAtom = useSetAtom(tokenAtom);
+  const setToken = useSetAtom(tokenAtom);
 
   const handleToken = async () => {
     try {
@@ -26,10 +26,10 @@ const ServiceWorker = () => {
         serviceWorkerRegistration: register,
       });
 
-      setTokenAtom(() => token);
+      setToken(() => token);
     } catch (error) {
       console.error(error);
-      setTokenAtom(() => undefined);
+      setToken(() => undefined);
     }
   };
 
