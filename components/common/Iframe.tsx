b4a1d7bcbf599bc1565876c8e81f8b09d834d72f
yiminwook <grs0416@gmail.com>
2023-12-05 09:50:11 +0900

    Update: append new v chan origin으로 theme 변경시 message 전달

--- components/common/Iframe.tsx
+++ components/common/Iframe.tsx
@@ -1,18 +1,52 @@
 'use client';
+import { themeAtom } from '@/atoms';
 import iframe from '@/components/common/iframe.module.scss';
 import { openWindow } from '@/utils/windowEvent';
-import { useRef } from 'react';
+import { useAtomValue } from 'jotai';
+import { useRef, useEffect } from 'react';
 
 interface IframeProps {
   url: string;
 }
 
+const CHILD_CHILD_ORIGIN = 'https://append-new-vchan.vercel.app';
+
 const Iframe = ({ url }: IframeProps) => {
   const iframeRef = useRef<HTMLIFrameElement>(null);
+  const theme = useAtomValue(themeAtom);
 
   const onClick = () => {
     openWindow(url);
   };
+
+  const postMsgToChild = () => {
+    const childWindow = iframeRef.current?.contentWindow;
+    if (!childWindow) return;
+
+    const msg = {
+      action: 'themeChange',
+      theme,
+    };
+
+    childWindow.postMessage(msg, CHILD_CHILD_ORIGIN);
+  };
+
+  const resiveMsgFromChild = (event: MessageEvent) => {
+    if (event.origin !== CHILD_CHILD_ORIGIN) return;
+    console.log('reciveFromChild', event.data);
+  };
+
+  useEffect(() => {
+    const child = iframeRef.current?.contentWindow;
+    if (!child) return;
+
+    window.addEventListener('message', resiveMsgFromChild);
+    () => window.removeEventListener('message', resiveMsgFromChild);
+  }, []);
+
+  useEffect(() => {
+    postMsgToChild();
+  }, [theme]);
 
   return (
     <section className={iframe['iframe']}>
