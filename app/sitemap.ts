f49510e6c43ae4f80cb335eaec7cebf8be47d3b7
yiminwook <grs0416@gmail.com>
2023-12-20 15:24:31 +0900

    Update: sitemap id

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,7 +2,7 @@ import { Promised } from '@/types';
 import { MetadataRoute } from 'next';
 
 interface SiteMapProps {
-  id: number;
+  id: string;
 }
 
 type SiteMap = {
@@ -17,17 +17,17 @@ type SiteMap = {
  */
 export async function generateSitemaps(): Promise<SiteMapProps[]> {
   // fetch를 통해 데이터를 가져와서 sitemap을 자동으로 구성하도록 만들수있다.
-  return [{ id: 0 }];
+  return [{ id: 'index' }];
 }
 
 export default function sitemap({ id }: Promised<typeof generateSitemaps>[0]): SiteMap[] {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL;
 
-  const sites = ['/', 'live', 'daily', 'all', 'search', 'channels', 'sheet/request', 'short', 'settings'];
+  const sites = ['/', '/live', '/daily', '/all', '/search', '/channels', '/sheet/request', '/short', '/settings'];
 
   return sites.map((site, index) => {
     return {
-      url: site !== '' ? `${baseUrl}/${site}` : `${baseUrl}`,
+      url: site !== '/' ? `${baseUrl}${site}` : `${baseUrl}`,
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.7,
