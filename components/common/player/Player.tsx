dc1a191c057452103c4c8be0130b66413197a9a1
yiminwook <grs0416@gmail.com>
2024-01-22 23:56:58 +0900

    Update: 음량조절 키보드 컨트롤 삭제

--- components/common/player/Player.tsx
+++ components/common/player/Player.tsx
@@ -17,7 +17,6 @@ const Player = ({ isShow }: PlayerProps) => {
   const player = useRef<ReactPlayer>(null);
   const [isReady, setIsReady] = useState(false);
   const [playerValue, setPlayerValue] = usePlayerAtom();
-  const [volume, setVolume] = useState(100);
   const toast = useToast();
 
   const keyDown = (e: KeyboardEvent) => {
@@ -38,30 +37,6 @@ const Player = ({ isShow }: PlayerProps) => {
         setPlayerValue((pre) => {
           toast.info({ text: `플레이어 ${pre.hide ? '보이기' : '숨기기'}`, duration: 2 });
           return { ...pre, hide: !pre.hide };
-        });
-      }
-      if (e.key === 'ArrowRight') {
-        setVolume((pre) => {
-          if (pre >= 100) {
-            toast.warning({ text: `최대 볼륨입니다.`, duration: 2 });
-            return 100;
-          }
-          toast.info({ text: `현재볼륨 ${pre}%`, duration: 2 });
-          return pre + 10;
-        });
-      }
-      if (e.key === 'ArrowLeft') {
-        // setVolume((pre) => {
-        //   if (pre <= 0) {
-        //     toast.warning({ text: `최소 볼륨입니다.`, duration: 2 });
-        //     return 0;
-        //   }
-        //   toast.info({ text: `현재볼륨 ${pre}%`, duration: 2 });
-        //   return pre - 10;
-        // });
-        player.current?.setState((pre: PlayerRefProps) => {
-          console.log(pre);
-          return { ...pre, volume: pre.volume + 0.1 };
         });
       }
     }
@@ -97,9 +72,8 @@ const Player = ({ isShow }: PlayerProps) => {
         playing={playerValue.isPlaying}
         onPlay={() => handlePlay(true)}
         onPause={() => handlePlay(false)}
-        config={{ youtube: { playerVars: { control: 1, suggestedQuality: 'hd720' } } }}
+        config={{ youtube: { playerVars: { suggestedQuality: 'hd720' } } }}
         controls={true}
-        volume={1}
         onReady={() => setIsReady(() => true)}
       />
       <PipButton className="hideButton" onClick={toggleLeft}>
