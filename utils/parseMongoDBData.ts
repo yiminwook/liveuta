206d56d5607edf8e65aa9a6fe0e094e0edb939f0
utawakuworker <136960279+utawakuworker@users.noreply.github.com>
2024-02-06 06:07:16 +0900

    Update parseMongoDBData.ts

--- utils/parseMongoDBData.ts
+++ utils/parseMongoDBData.ts
@@ -8,7 +8,7 @@ interface ContentDocument {
     Title: string;
     URL: string;
     ChannelName: string;
-    ScheduledTime: Date;
+    ScheduledTime: string;
     ThumbnailURL: string;
     Hide: string;
     broadcastStatus: string;
@@ -22,7 +22,7 @@ interface DocumentList {
 export const parseMongoDBDocument = (doc: ContentDocument): ContentsDataType | undefined => {
   try {
     const { _id, Title, URL, ChannelName, ScheduledTime, ThumbnailURL, broadcastStatus, isVideo } = doc;
-    const { timestamp, korTime } = stringToTime(ScheduledTime.toISOString());
+    const { timestamp, korTime } = stringToTime(ScheduledTime);
     console.log(timestamp);
     console.log(korTime);
     const interval = getInterval(timestamp);
