bcb825019da8dee08d45035163d25e42751732b0
utawakuworker <136960279+utawakuworker@users.noreply.github.com>
2024-02-06 05:22:38 +0900

    Create inMongoDB.ts

---
+++ types/inMongoDB.ts
@@ -0,0 +1,12 @@
+export interface ContentsDataType {
+  title: string;
+  url: string;
+  channelName: string;
+  videoId: string;
+  timestamp: number;
+  thumbnailURL?: string;
+  isStream: isStream;
+  korTime: string;
+  interval: string;
+  isVideo: boolean;
+}
