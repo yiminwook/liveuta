7818a907c9c8d999733b6d4d3f3c854fb75b4c12
utawakuworker <136960279+utawakuworker@users.noreply.github.com>
2024-02-08 05:06:46 +0900

    Update page.client.tsx

--- app/(inner)/(pip)/page.client.tsx
+++ app/(inner)/(pip)/page.client.tsx
@@ -1,22 +1,21 @@
 'use client';
 import { useEffect } from 'react';
-import useSheet from '@/queries/sheet';
+//import useSheet from '@/queries/sheet';
 import NavSection from '@/components/home/NavSection';
 import ScheduleSection from '@/components/home/ScheduleSection';
 import TopSection from '@/components/home/TopSection';
 import useResponsive from '@/hooks/useResponsive';
 import clientOnly from '@/models/clientOnly';
-import { SheetAPIReturntype } from '@/types/inSheet';
+//import { SheetAPIReturntype } from '@/types/inSheet';
 import useMongoDB from '@/queries/mongoDBService';
+import { MongoDBAPIReturntype } from '@/types/inMongoDB';
 
 interface MainProps {
-  filter: keyof SheetAPIReturntype;
+  filter: keyof MongoDBAPIReturntype;
 }
 
 const Main = ({ filter }: MainProps) => {
-  const mongoDBResult = useMongoDB();
-  console.log('MongoDB Result:', mongoDBResult);
-  //const { contents, isLoad } = useSheet(filter);
+  const { contents, isLoad } = useMongoDB(filter);
   const { isMobile, isDesktop } = useResponsive();
 
   return (
