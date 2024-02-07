672df7d92cd2951769a594481a48935ac482e045
utawakuworker <136960279+utawakuworker@users.noreply.github.com>
2024-02-08 05:28:08 +0900

    Update ScheduleSection.tsx

--- components/home/ScheduleSection.tsx
+++ components/home/ScheduleSection.tsx
@@ -1,4 +1,4 @@
-import { ContentsDataType } from '@/types/inSheet';
+import { ContentsDataType } from '@/types/inMongoDB';
 import ScheduleCard from '@/components/common/scheduleCard/Card';
 import { useEffect, useMemo, useState } from 'react';
 import { SCROLL_PER_YOUTUBE_CARD } from '@/consts';
