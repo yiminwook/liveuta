8bfb449c1e17f04a01d42e94d188e53bb1149d82
utawakuworker <136960279+utawakuworker@users.noreply.github.com>
2024-02-08 05:06:03 +0900

    Update index.ts

--- queries/mongoDBService/index.ts
+++ queries/mongoDBService/index.ts
@@ -4,21 +4,20 @@ import { fetcher } from '@/queries';
 import { useQuery } from '@tanstack/react-query';
 import { useSetAtom } from 'jotai';
 import { isLoadingSheetAtom } from '@/atoms';
+import { ContentsDataType, MongoDBAPIReturntype } from '@/types/inMongoDB';
 
 const MONGODB_REFRESH_INTERVAL = 1000 * 60 * 3; // 3 minutes
 
 // NOTE: Temporary data types set to 'any'. Modify types after transitioning to MongoDB service.
 const useMongoDB = () => {
   const [isLoad, setIsLoad] = useState(false);
-  const [contents, setContents] = useState<any[]>([]);
+  const [contents, setContents] = useState<ContentsDataType[]>([]);
   const setIsLoadingSheet = useSetAtom(isLoadingSheetAtom);
 
-  const { data, dataUpdatedAt, isLoading, refetch, status } = useQuery<any>({
+  const { data, dataUpdatedAt, isLoading, refetch, status } = useQuery<MongoDBAPIReturntype>({
     queryKey: ['mongodb'],
     queryFn: async () => {
-      const result = await fetcher('/api/mongoDBService');
-      //console.log('Fetcher result:', result); // Logging the result
-      return result;
+      return result = await fetcher('/api/mongoDBService');
     },
     refetchInterval: MONGODB_REFRESH_INTERVAL,
     staleTime: 1000 * 60, // 1 minute
@@ -28,26 +27,35 @@ const useMongoDB = () => {
     refetchIntervalInBackground: false,
   });
 
-  useEffect(() => {
-    if (data && status === 'success') {
-      setContents(data.contents);
-      setIsLoad(true);
-    }
-  }, [dataUpdatedAt]);
+  const setData = () => {
+    if (!data) return;
+    const contents = [...data[filter].contents];
+    setContents(() => contents);
+  };
 
   useEffect(() => {
-    setIsLoadingSheet(isLoading);
+    if (data && status === 'success') {
+      //onSuccess
+      setData();
+      setIsLoad(() => true);
+    }
+  }, [dataUpdatedAt, filter]);
+
+  useEffect(() => {
+    setIsLoadingSheet(() => isLoading);
   }, [isLoading]);
 
   return {
+    /** 필터링된 데이터 */
     contents,
-    data,
+    /** 원본 데이터 */
+    sheetData: data,
+    filter,
     isLoadingSheet: isLoading,
     refetchSheet: refetch,
     sheetDataUpdatedAt: dataUpdatedAt,
     isLoad,
   };
 };
-
 
 export default useMongoDB;
