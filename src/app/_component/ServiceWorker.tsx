727a93f049afa78a7ebd406f08623d6e40687ff1
yiminwook <grs0416@gmail.com>
2024-02-09 21:10:20 +0900

    Test: FCM token

--- src/app/_component/ServiceWorker.tsx
+++ src/app/_component/ServiceWorker.tsx
@@ -1,42 +1,60 @@
 'use client';
 import FirebaseClient from '@/model/firebase/client';
 import { generateFcmToken } from '@/model/firebase/generateFcmToken';
+import { useQuery } from '@tanstack/react-query';
 import { onMessage } from 'firebase/messaging';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function ServiceWorker() {
-  const handleMessage = async () => {
-    try {
-      const token = await generateFcmToken();
-      alert(token);
+  const handleMessage = () => {
+    const messaging = FirebaseClient.getInstance().message;
+    onMessage(messaging, ({ data, from, collapseKey, messageId }) => {
+      if (data === undefined) return;
 
-      const messaging = FirebaseClient.getInstance().message;
-      onMessage(messaging, ({ data, from, collapseKey, messageId }) => {
-        if (data === undefined) return;
-
-        const noti = new Notification(data.title!, {
-          body: data.body,
-          image: data.imageUrl!,
-          timestamp: Number(data.timestamp),
-        });
-
-        noti.onclick = () => {
-          const url = window.location.href;
-          if (url !== data.link) {
-            window.open(data.link);
-          }
-          noti.close();
-        };
+      const noti = new Notification(data.title!, {
+        body: data.body,
+        image: data.imageUrl!,
+        timestamp: Number(data.timestamp),
       });
-    } catch (error) {
-      alert(error);
-      console.error(error);
-    }
+
+      noti.onclick = () => {
+        const url = window.location.href;
+        if (url !== data.link) {
+          window.open(data.link);
+        }
+        noti.close();
+      };
+    });
   };
 
+  const { status, dataUpdatedAt } = useQuery({
+    queryKey: ['fcmToken'],
+    queryFn: async () => {
+      try {
+        const token = await generateFcmToken();
+        if (token === undefined) throw new Error('Token is undefined');
+        alert(token);
+        return token;
+      } catch (error) {
+        alert(error);
+        throw error;
+      }
+    },
+    retry: 5,
+    retryDelay: 2000,
+    refetchOnWindowFocus: true,
+    refetchOnMount: true,
+    refetchOnReconnect: true,
+    staleTime: 1000 * 60 * 5,
+    gcTime: 1000 * 60 * 5,
+  });
+
   useEffect(() => {
-    handleMessage();
-  }, []);
+    if (status === 'success') {
+      handleMessage();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dataUpdatedAt]);
 
   return <></>;
 }
