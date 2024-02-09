af07ab43897e7f5cdcc92dfddadfecc05ee6a63f
yiminwook <grs0416@gmail.com>
2024-02-10 02:28:33 +0900

    Test: ServiceWorker 변경사항 롤백

--- src/app/_component/ServiceWorker.tsx
+++ src/app/_component/ServiceWorker.tsx
@@ -2,23 +2,16 @@
 import FirebaseClient from '@/model/firebase/client';
 import { generateFcmToken } from '@/model/firebase/generateFcmToken';
 import { onMessage } from 'firebase/messaging';
-import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
 export default function ServiceWorker() {
-  const router = useRouter();
-  const [load, setLoad] = useState(false);
-
   const handleMessage = async () => {
     try {
-      console.log('req');
       const token = await generateFcmToken();
 
       if (token === undefined) {
         throw new Error('브라우저 알림허용 설정이 되어있지 않습니다.');
       }
-
-      alert(token);
 
       const messaging = FirebaseClient.getInstance().message;
       onMessage(messaging, ({ data, from, collapseKey, messageId }) => {
@@ -39,20 +32,14 @@ export default function ServiceWorker() {
         };
       });
     } catch (error) {
-      console.error('handleMessage', error);
-      alert(error);
+      console.error('FCM: ', error);
     }
   };
 
   useEffect(() => {
-    if (!load) {
-      console.log('load');
-      router.refresh();
-      return setLoad(() => true);
-    }
     handleMessage();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [load]);
+  }, []);
 
   return <></>;
 }
