9ea0d6dbb268a533f70722a9f34e9aff50573794
yiminwook <grs0416@gmail.com>
2024-02-09 22:13:05 +0900

    Update: ServiceWorker 변경사항 롤백

--- src/app/_component/ServiceWorker.tsx
+++ src/app/_component/ServiceWorker.tsx
@@ -1,61 +1,41 @@
 'use client';
-import clientOnly from '@/model/clientOnly';
 import FirebaseClient from '@/model/firebase/client';
 import { generateFcmToken } from '@/model/firebase/generateFcmToken';
-import { useQuery } from '@tanstack/react-query';
 import { onMessage } from 'firebase/messaging';
-import { PropsWithChildren, useEffect } from 'react';
+import { useEffect } from 'react';
 
-export default clientOnly(function ServiceWorker({ children }: PropsWithChildren) {
-  const handleMessage = () => {
-    const messaging = FirebaseClient.getInstance().message;
-    onMessage(messaging, ({ data, from, collapseKey, messageId }) => {
-      if (data === undefined) return;
+export default function ServiceWorker() {
+  const handleMessage = async () => {
+    try {
+      const token = await generateFcmToken();
+      alert(token);
 
-      const noti = new Notification(data.title!, {
-        body: data.body,
-        image: data.imageUrl!,
-        timestamp: Number(data.timestamp),
+      const messaging = FirebaseClient.getInstance().message;
+      onMessage(messaging, ({ data, from, collapseKey, messageId }) => {
+        if (data === undefined) return;
+
+        const noti = new Notification(data.title!, {
+          body: data.body,
+          image: data.imageUrl!,
+          timestamp: Number(data.timestamp),
+        });
+
+        noti.onclick = () => {
+          const url = window.location.href;
+          if (url !== data.link) {
+            window.open(data.link);
+          }
+          noti.close();
+        };
       });
-
-      noti.onclick = () => {
-        const url = window.location.href;
-        if (url !== data.link) {
-          window.open(data.link);
-        }
-        noti.close();
-      };
-    });
+    } catch (error) {
+      console.error(error);
+    }
   };
 
-  const { status, isPending } = useQuery({
-    queryKey: ['fcmToken'],
-    queryFn: async () => {
-      try {
-        const token = await generateFcmToken();
-        if (token === undefined) throw new Error('Token is undefined');
-        alert(token);
-        return token;
-      } catch (error) {
-        alert(error);
-        throw error;
-      }
-    },
-    retry: 5,
-    retryDelay: 2000,
-    refetchOnWindowFocus: true,
-    refetchOnMount: true,
-    refetchOnReconnect: true,
-    staleTime: 1000 * 60 * 5,
-    gcTime: 1000 * 60 * 5,
-  });
-
   useEffect(() => {
-    if (status === 'success') {
-      handleMessage();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isPending]);
+    handleMessage();
+  }, []);
 
   return <></>;
-});
+}
