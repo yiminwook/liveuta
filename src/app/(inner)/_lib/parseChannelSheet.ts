80802a941799660e3192c319ca8195845c0fdbf8
yiminwook <grs0416@gmail.com>
2024-02-21 15:17:10 +0900

    Update: 채널 페이지 name_kor 기준으로 정렬

--- src/app/(inner)/_lib/parseChannelSheet.ts
+++ src/app/(inner)/_lib/parseChannelSheet.ts
@@ -1,21 +1,13 @@
 import { readDB } from '@/model/mongoDBService';
 import { ChannelDocument } from '@/type/api/mongoDB';
 
-/** Parse Google spread sheet - reference */
 export const parseChannelIDSheet = async () => {
   const collection = process.env.MONGODB_CHANNEL_COLLECTION;
   const database = process.env.MONGODB_CHANNEL_DB;
-  const sheetDataValues: ChannelDocument[] = await readDB(collection, database);
+  const sheetDataValues: ChannelDocument[] = await readDB(collection, database, {
+    sort: { name_kor: 1 },
+  });
 
   const totalLength = sheetDataValues.length;
   return { totalLength, sheetDataValues };
 };
-
-// const sheetData = await getSheet({
-//   spreadsheetId: process.env.CHANNELS_SHEET_ID,
-//   range: process.env.CHANNELS_SHEET_RANGE,
-// });
-// const sheetDataValues = sheetData.values as ChannelRowType[];
-// if (!(sheetDataValues && sheetDataValues.length >= 2))
-//   throw new Error('sheetData has not values');
-// sheetDataValues.shift();
