815b55d676082e7298ab20e540ba100b46af7a58
yiminwook <grs0416@gmail.com>
2024-02-24 23:54:09 +0900

    Fix: schedule query가 없을때 useScheduleStatus undefined 반환

--- src/hook/useScheduleStatus.ts
+++ src/hook/useScheduleStatus.ts
@@ -1,8 +1,9 @@
 import { useQueryClient } from '@tanstack/react-query';
 
+/** query observer가 없을때 undefined 반환 */
 const useScheduleStatus = () => {
   const queryClient = useQueryClient();
-  return queryClient.getQueryState(['schedule'])?.status || 'pending';
+  return queryClient.getQueryState(['schedule'])?.status;
 };
 
 export default useScheduleStatus;
