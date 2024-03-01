c7f943e2aae74b41da7c5bdd2a36b84881b74325
yiminwook <grs0416@gmail.com>
2024-03-02 04:57:56 +0900

    Feat: getViewer proxy url

--- src/app/(inner)/_lib/getViewer.ts
+++ src/app/(inner)/_lib/getViewer.ts
@@ -1,19 +1,17 @@
-'use server';
 import axios from 'axios';
 
 export async function getViewer(id: string) {
-  try {
-    const res = await axios({
-      method: 'GET',
-      url: `https://youtube.com/watch?v=${id}`,
-    });
+  const yturl = `https://youtube.com/watch?v=${id}`;
+  const res = await axios({
+    method: 'GET',
+    url: `https://proxy.grs0412.workers.dev/${yturl}`,
+    headers: {
+      Authorization: 'token',
+    },
+  });
 
-    const pattern = /\{"text"\:"현재 "\},\{"text"\:"(\d+(?:,\d{3})*)"\},\{"text"\:"명 시청 중"\}/;
-    const count = res.data.match(pattern)?.[1];
+  const pattern = /\{"text"\:"현재 "\},\{"text"\:"(\d+(?:,\d{3})*)"\},\{"text"\:"명 시청 중"\}/;
+  const count = res.data.match(pattern)?.[1];
 
-    return typeof count !== 'string' ? '?' : count;
-  } catch (error) {
-    console.log('viewer ServerAction', error);
-    return '?';
-  }
+  return typeof count !== 'string' ? '?' : count;
 }
