cf9d40e0a880d62a63ff5b9baf2476020d818a25
yiminwook <grs0416@gmail.com>
2024-03-06 20:01:13 +0900

    Fix: select cache 초기화

--- src/app/(inner)/_component/header/NavSelectBox.tsx
+++ src/app/(inner)/_component/header/NavSelectBox.tsx
@@ -7,11 +7,12 @@ import { BiArrowFromLeft } from 'react-icons/bi';
 import { BsSliders } from 'react-icons/bs';
 import OutsideClickHandler from 'react-outside-click-handler';
 import { SelectType } from '@/type';
-import { useSelectAtom, useSelectedScheduleAtom } from '@inner/_lib/atom';
+import { useRouter } from 'next/navigation';
 
 interface NavSelectBoxProps {}
 
 export default function NavSelectBox({}: NavSelectBoxProps) {
+  const router = useRouter();
   const [active, setActive] = useState(false);
   const [select, setSelect] = useSelectAtom();
   const [selectedSchedule] = useSelectedScheduleAtom();
@@ -27,6 +28,8 @@ export default function NavSelectBox({}: NavSelectBoxProps) {
       selectCookie.set('select', li, { path: '/', maxAge: 60 * 60 * 24 * 30 * 3 }); //3개월 저장
       setSelect(() => li);
       setActive(() => false);
+      // select cache 삭제
+      router.refresh();
     }
   };
 
