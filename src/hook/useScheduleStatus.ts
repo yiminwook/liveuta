09a7923684e392c9620a3debad478ddecc3869c8
yiminwook <grs0416@gmail.com>
2024-03-08 11:35:37 +0900

    Update: useScheduleStatus

--- src/hook/useScheduleStatus.ts
+++ src/hook/useScheduleStatus.ts
@@ -3,7 +3,7 @@ import { useIsFetching, useQueryClient } from '@tanstack/react-query';
 /** query observer가 없을때 undefined 반환 */
 const useScheduleStatus = () => {
   const queryClient = useQueryClient();
-  useIsFetching(); //리랜더링용
+  useIsFetching({ queryKey: ['schedule'] }); //리랜더링용
   return queryClient.getQueryState(['schedule'])?.status;
 };
 
