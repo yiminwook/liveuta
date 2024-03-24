9519fec9ec1f1a7d1a7f27735e3802ef5ce5a389
yiminwook <grs0416@gmail.com>
2024-03-24 22:40:31 +0900

    Update: 로딩토스트 최대시간 7초

--- src/app/(inner)/layout.data.tsx
+++ src/app/(inner)/layout.data.tsx
@@ -77,6 +77,11 @@ export default function LayoutDataObserver({ session }: LayoutDataObserverProps)
       toast.loading('서버와 통신중입니다.', {
         id: 'loading',
       });
+      const timer = setTimeout(() => {
+        // 로딩토스트가 7초이상 보이지 않도록 함
+        toast.dismiss('loading');
+      }, 7000);
+      return () => clearTimeout(timer);
     } else {
       toast.dismiss('loading');
     }
