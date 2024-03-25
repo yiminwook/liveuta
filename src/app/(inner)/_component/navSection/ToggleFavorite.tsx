474d258eff397d627dcaed74384efea6a1777683
yiminwook <grs0416@gmail.com>
2024-03-25 22:34:51 +0900

    Update: toggleFavorite localStorage 적용

--- src/app/(inner)/_component/navSection/ToggleFavorite.tsx
+++ src/app/(inner)/_component/navSection/ToggleFavorite.tsx
@@ -1,16 +1,31 @@
 'use client';
 import { toggleBlacklistAtom } from '@inner/_lib/atom/schedule';
 import { useAtom } from 'jotai';
+import { useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
 import * as styles from './navSection.css';
 
 export default function ToggleFavorite() {
-  const [toggle, setToggle] = useAtom(toggleBlacklistAtom);
+  const [toggleBlacklist, setToggleBlacklist] = useAtom(toggleBlacklistAtom);
 
-  const handleToggle = () => setToggle((pre) => !pre);
+  const handleToggle = () => {
+    setToggleBlacklist((pre) => {
+      localStorage.setItem('favorite', pre ? 'true' : 'false');
+      return !pre;
+    });
+  };
+
+  useEffect(() => {
+    setToggleBlacklist(() => {
+      const result = localStorage.getItem('favorite') !== 'true';
+      return result;
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <button className={styles.favoriteButton} onClick={handleToggle}>
-      <FaStar size="1.2rem" color={!toggle ? '#ffbb00' : '#a7a7a7'} />
+      <FaStar size="1.2rem" color={!toggleBlacklist ? '#ffbb00' : '#a7a7a7'} />
     </button>
   );
 }
