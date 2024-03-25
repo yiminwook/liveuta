962bcf5e23f9a3f5f4a673663ee71b2fa6f3913d
yiminwook <grs0416@gmail.com>
2024-03-25 21:22:05 +0900

    Update: layout ssr

--- src/app/_component/index.tsx
+++ src/app/_component/index.tsx
@@ -1,23 +1,21 @@
 import { PORTAL_ID } from '@/const';
 import { GetCookiesReturnType } from '@inner/_lib/getCookie';
-import dynamic from 'next/dynamic';
 import Devtools from './Devtools';
 import GlobalHydrate from './GlobalHydrate';
 import Hotkeys from './Hotkeys';
 import Jotai from './Jotai';
+import ModalProvider from './ModalProvider';
 import NextAuth from './NextAuth';
+import ParticleProvider from './ParticleProvider';
 import ReactQuery from './ReactQuery';
 import ServiceWorker from './ServiceWorker';
 import ThemeProvider from './ThemeProvider';
 import ToastBox from './ToastBox';
 
-const ModalProvider = dynamic(() => import('./ModalProvider'), { ssr: false });
-const ParticleProvider = dynamic(() => import('./ParticleProvider'), { ssr: false });
-
-interface ConfigsProps {
+type ConfigsProps = {
   children: React.ReactNode;
   cookies: GetCookiesReturnType;
-}
+};
 
 export default function Configs({ children, cookies }: ConfigsProps) {
   return (
