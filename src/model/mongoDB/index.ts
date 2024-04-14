76dd1d87f824d0e0285d4c2d2c63df9d546e7f02
yiminwook <grs0416@gmail.com>
2024-04-14 19:10:25 +0900

    Update:  mongoDB maxPoolSize

--- src/model/mongoDB/index.ts
+++ src/model/mongoDB/index.ts
@@ -1,6 +1,6 @@
 import { MongoClient } from 'mongodb';
 
-export const mongoDB = new MongoClient(process.env.MONGODB_URI);
+export const mongoDB = new MongoClient(process.env.MONGODB_URI, { maxPoolSize: 100 });
 
 export const connectMongoDB = async (dbName: string, collectionName: string) => {
   const client = await mongoDB.connect();
