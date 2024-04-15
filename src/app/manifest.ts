1043424f1067c682fbcd8261f8185a6852611a4b
yiminwook <grs0416@gmail.com>
2024-04-15 23:17:32 +0900

    Update: manifest orientation

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -8,7 +8,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     scope: '/',
     display: 'standalone',
-    orientation: 'portrait-primary',
     icons: [
       {
         src: '/assets/icon-192-192.png',
