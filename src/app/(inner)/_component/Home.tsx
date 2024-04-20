35fafeb6c07313cf8dab98c3f8958c10489b91ec
yiminwook <grs0416@gmail.com>
2024-04-20 22:27:21 +0900

    Update: schedule hydate 제거
    1. package 업그레이드

--- src/app/(inner)/_component/Home.tsx
+++ src/app/(inner)/_component/Home.tsx
@@ -1,10 +1,6 @@
-import getQueryClient from '@/app/_lib/getQueryClient';
 import { auth } from '@/model/nextAuth';
 import { ScheduleAPIReturntype } from '@/type/api/mongoDB';
-import { GetScheduleRes } from '@api/schedule/type';
 import { getCookies } from '@inner/_lib/getCookie';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
-import axios from 'axios';
 import dynamic from 'next/dynamic';
 import Background from './Background';
 import HomeDataObserver from './Home.data';
@@ -22,26 +18,12 @@ export default async function Home({ filter, query }: HomeProps) {
   const { select } = await getCookies();
   const session = await auth();
 
-  const queryClient = getQueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['schedule'],
-    queryFn: () =>
-      axios
-        .get<GetScheduleRes>(`${process.env.NEXT_PUBLIC_SITE_URL}/api/schedule`)
-        .then((res) => res.data.data),
-  });
-
-  const dehydratedState = dehydrate(queryClient);
-
   return (
-    <HydrationBoundary state={dehydratedState}>
-      <Background tile={true}>
-        <HomeDataObserver filter={filter} select={select} query={query} />
-        <NavSection />
-        <TopSection filter={filter} />
-        <ScheduleSection session={session} />
-      </Background>
-    </HydrationBoundary>
+    <Background tile={true}>
+      <HomeDataObserver filter={filter} select={select} query={query} />
+      <NavSection />
+      <TopSection filter={filter} />
+      <ScheduleSection session={session} />
+    </Background>
   );
 }
