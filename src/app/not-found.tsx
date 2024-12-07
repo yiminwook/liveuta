d5b7f02384a94c1ec7f9a424ce755476a3ce0357
thinline20 <pekochan069@gmail.com>
2024-12-07 17:55:51 +0900

    Refactor: Error styles to scss

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import character from '@/assets/image/character-8.png';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
-import * as styles from './not-found.css';
+import css from './not-found.module.scss';
 
 export const metadata: Metadata = {
   title: '404: Not Found',
@@ -13,26 +13,22 @@ export default function NotFound() {
   const router = useRouter();
 
   return (
-    <div className={styles.wrap}>
-      <div className={styles.inner}>
-        <div className={styles.imgBox}>
-          <Image
-            src={character}
-            alt="페이지를 찾을 수 없습니다."
-            width={200}
-            height={300}
-            unoptimized={true}
-          />
-        </div>
-        <div className={styles.desc}>
-          <div className={styles.descTop}>
+    <div className={css.wrap}>
+      <div className={css.box}>
+        <Image
+          src={character}
+          alt="페이지를 찾을 수 없습니다."
+          width={200}
+          height={300}
+          unoptimized={true}
+        />
+        <div className={css.desc}>
+          <div className={css.descTop}>
             <h1>404: Not Found</h1>
             <h2>페이지를 찾을 수 없습니다.</h2>
           </div>
-          <div className={styles.descBottom}>
-            <button className={styles.button} onClick={() => router.back()}>
-              뒤로가기
-            </button>
+          <div className={css.descBottom}>
+            <button onClick={() => router.back()}>뒤로가기</button>
           </div>
         </div>
       </div>
