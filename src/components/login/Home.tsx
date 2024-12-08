c066ff812982568219080d6df0bdbc8a28fc3f09
thinline20 <pekochan069@gmail.com>
2024-12-09 00:32:45 +0900

    Refactor: Login styles to scss

--- src/components/login/Home.tsx
+++ src/components/login/Home.tsx
@@ -1,15 +1,15 @@
 'use client';
-import { signIn } from 'next-auth/react';
-import { useMutation } from '@tanstack/react-query';
-import * as styles from './home.css';
-import { FcGoogle } from 'react-icons/fc';
-import { RiKakaoTalkFill } from 'react-icons/ri';
-import { Provider } from '@/types/nextAuth';
-import { FaDiscord } from 'react-icons/fa';
-import { toast } from 'sonner';
-import Image from 'next/image';
 import character from '@/assets/image/character-3.png';
 import Background from '@/components/common/Background';
+import { Provider } from '@/types/nextAuth';
+import { useMutation } from '@tanstack/react-query';
+import { signIn } from 'next-auth/react';
+import Image from 'next/image';
+import { FaDiscord } from 'react-icons/fa';
+import { FcGoogle } from 'react-icons/fc';
+import { RiKakaoTalkFill } from 'react-icons/ri';
+import { toast } from 'sonner';
+import css from './Home.module.scss';
 
 export default function Home() {
   const mutateLogin = useMutation({
@@ -20,10 +20,10 @@ export default function Home() {
 
   return (
     <Background>
-      <div className={styles.wrap}>
+      <div className={css.wrap}>
         <h1 className="blind">로그인 페이지</h1>
-        <div className={styles.inner}>
-          <div className={styles.imgBox}>
+        <div className={css.inner}>
+          <div className={css.imgBox}>
             <Image
               src={character}
               width={200}
@@ -33,23 +33,23 @@ export default function Home() {
             />
           </div>
           <p>소셜 계정으로 시작</p>
-          <div className={styles.buttonBox}>
+          <div className={css.buttonBox}>
             <button
-              className={styles.googleLoginButton}
+              className={css.socialLoginButton}
               onClick={() => mutateLogin.mutate('google')}
               disabled={mutateLogin.isPending}
             >
               <FcGoogle size={28} />
             </button>
             <button
-              className={styles.googleLoginButton}
+              className={css.socialLoginButton}
               onClick={() => mutateLogin.mutate('kakao')}
               disabled={mutateLogin.isPending}
             >
               <RiKakaoTalkFill size={28} />
             </button>
             <button
-              className={styles.googleLoginButton}
+              className={css.socialLoginButton}
               onClick={() => mutateLogin.mutate('discord')}
               disabled={mutateLogin.isPending}
             >
