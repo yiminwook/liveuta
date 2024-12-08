1391c45598b962ae14d21f8d333f3bfb497aa390
thinline20 <pekochan069@gmail.com>
2024-12-09 00:32:58 +0900

    Refactor: Multi styles to scss

--- src/components/multi/Home.tsx
+++ src/components/multi/Home.tsx
@@ -1,10 +1,11 @@
+import css from './Home.module.scss';
 import Nav from './Nav';
 import ShortsSection from './ShortsSection';
 import * as styles from './home.css';
 
 export default async function Home() {
   return (
-    <div className={styles.wrap}>
+    <div className={css.wrap}>
       <Nav />
       <ShortsSection />
     </div>
--- src/components/multi/Nav.tsx
+++ src/components/multi/Nav.tsx
@@ -1,8 +1,8 @@
 'use client';
-import useModalStore from '@/hooks/useModalStore';
-import * as styles from './nav.css';
-import { FaPlus } from 'react-icons/fa6';
 import ListModal from '@/components/common/modal/MultiListModal';
+import useModalStore from '@/hooks/useModalStore';
+import { FaPlus } from 'react-icons/fa6';
+import css from './Nav.module.scss';
 
 export default function Nav() {
   const modalStore = useModalStore();
@@ -10,7 +10,7 @@ export default function Nav() {
     await modalStore.push(ListModal, {});
   };
   return (
-    <button className={styles.wrap} onClick={open}>
+    <button className={css.wrap} onClick={open}>
       <FaPlus size="1.5rem" />
     </button>
   );
