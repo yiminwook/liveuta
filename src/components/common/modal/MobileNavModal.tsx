5bc7a1499b544a7a68decb0d536962a9cb221b26
Minwook Yi <grs0416@gmail.com>
2024-12-08 18:29:06 +0900

    Update: MobileNavModal style

--- src/components/common/modal/MobileNavModal.tsx
+++ src/components/common/modal/MobileNavModal.tsx
@@ -1,12 +1,15 @@
 import SearchInput from '@/components/common/input/SearchInput';
 import VideoTypeRadio from '@/components/schedule//VideoTypeRadio';
 import NavTab from '@/components/schedule/NavTab';
+import { useTransition } from '@/hooks/useTransition';
 import { ModalBaseProps } from '@/libraries/modal/ModalController';
 import { TScheduleDto } from '@/types/dto';
+import { CloseButton } from '@mantine/core';
+import classNames from 'classnames';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { useState } from 'react';
+import css from './MobileNavModal.module.scss';
 import Modal from './Modal';
-import * as styles from './mobileNavModal.css';
 
 const MOBILE_NAV_MODAL_ID = 'mobileNavModal';
 
@@ -22,6 +25,7 @@ type MobileNavModalProps = {
 export default function MobileNavModal({ onClose, scheduleDto, length }: MobileNavModalProps) {
   const [query, setQuery] = useState(scheduleDto.query);
   const searchParams = useSearchParams();
+  const { modifier, onAnimationEnd, exit } = useTransition();
   const router = useRouter();
 
   const onChangeQuery = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,11 +39,23 @@ export default function MobileNavModal({ onClose, scheduleDto, length }: MobileN
     router.push(`/schedule?${params.toString()}`);
   };
 
+  const onCloseWithExit = () => {
+    exit(() => onClose());
+  };
+
   return (
-    <Modal id={MOBILE_NAV_MODAL_ID} onClose={onClose}>
-      <div className={styles.content}>
-        <div className={styles.navTabBox}>
+    <Modal
+      id={MOBILE_NAV_MODAL_ID}
+      className={classNames(modifier)}
+      onClose={onCloseWithExit}
+      onAnimationEnd={onAnimationEnd}
+      width={750}
+      withCloseButton={false}
+    >
+      <div className={css.content}>
+        <div className={css.navTabBox}>
           <NavTab filter={scheduleDto.filter} />
+          <CloseButton w={40} h={40} onClick={onCloseWithExit} />
         </div>
         <SearchInput
           placeholder="채널명으로 검색"
