2c52295244fbf6e2b82885801292c1a93af19289
thinline20 <pekochan069@gmail.com>
2024-12-12 02:04:17 +0900

    Add NProgress bar

---
+++ src/components/config/NProgress.tsx
@@ -0,0 +1,29 @@
+// Create a Providers component to wrap your application with all the components requiring 'use client', such as next-nprogress-bar or your different contexts...
+'use client';
+
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar';
+import { PropsWithChildren } from 'react';
+
+const NProgressProviders = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      {children}
+      <ProgressBar
+        options={{
+          showSpinner: false,
+          // template:
+          // '<div class="bar" role="bar"><div class="peg"></div></div><div class="spinner" role="spinner"><div class="spinner-icon"></div></div>', Default template
+        }}
+        shallowRouting
+      />
+      {/* <div className="bar" role="bar">
+        <div className="peg"></div>
+      </div>
+      <div className="spinner" role="spinner">
+        <div className="spinner-icon"></div>
+      </div> */}
+    </>
+  );
+};
+
+export default NProgressProviders;
