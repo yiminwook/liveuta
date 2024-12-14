e18bdd83b5add496965ac5757c4a746e967e29e9
thinline20 <pekochan069@gmail.com>
2024-12-14 22:28:25 +0900

    Fix modal not showing

--- src/components/config/ModalProvider.tsx
+++ src/components/config/ModalProvider.tsx
@@ -5,6 +5,7 @@ import ModalController from '@/libraries/modal/ModalController';
 import { PropsWithChildren, useState } from 'react';
 
 export default function ModalProvider({ children }: PropsWithChildren) {
+  'use no memo';
   const renderState = useState(1);
   const [modalController] = useState(() => new ModalController(renderState));
 
