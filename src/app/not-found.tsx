3053cfbf88695ee549678fd748f561732e6a1bb4
Minwook Yi <grs0416@gmail.com>
2024-12-15 21:45:07 +0900

    Fix: client error

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,38 +1,10 @@
-'use client';
-import character from '@/assets/image/character-8.png';
 import type { Metadata } from 'next';
-import { useRouter } from 'next-nprogress-bar';
-import { useTransitionRouter } from 'next-view-transitions';
-import Image from 'next/image';
-import css from './not-found.module.scss';
+import Client from './not-found.client';
 
 export const metadata: Metadata = {
   title: '404: Not Found',
 };
 
 export default function NotFound() {
-  const router = useRouter(useTransitionRouter);
-
-  return (
-    <div className={css.wrap}>
-      <div className={css.box}>
-        <Image
-          src={character}
-          alt="페이지를 찾을 수 없습니다."
-          width={200}
-          height={300}
-          unoptimized={true}
-        />
-        <div className={css.desc}>
-          <div className={css.descTop}>
-            <h1>404: Not Found</h1>
-            <h2>페이지를 찾을 수 없습니다.</h2>
-          </div>
-          <div className={css.descBottom}>
-            <button onClick={() => router.back()}>뒤로가기</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <Client />;
 }
