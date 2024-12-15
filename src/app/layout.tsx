a14e0ecf5b39c20552ca94da58b7ca921f3f5075
thinline20 <pekochan069@gmail.com>
2024-12-16 00:36:19 +0900

    Fix typo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ import Configs from '@/components/config';
 import DefaultHead from '@/components/config/DefaultHead';
 import GlobalScrollbar from '@/components/config/GlobalScrollbar';
 import GoogleTagManager from '@/components/config/GoogleTagManager';
-import { DEFALUT_METADATA } from '@/constants/metaData';
+import { DEFAULT_METADATA } from '@/constants/metaData';
 import { getCookies } from '@/utils/getCookie';
 import { isDarkModeEnabled } from '@/utils/helper';
 import type { Metadata, Viewport } from 'next';
@@ -54,7 +54,7 @@ export default async function RootLayout({ children }: PropsWithChildren) {
 /** Default Route Segment Config */
 export const dynamicParams = true; //fallback
 export const preferredRegion = ['icn1'];
-export const metadata: Metadata = DEFALUT_METADATA;
+export const metadata: Metadata = DEFAULT_METADATA;
 export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
