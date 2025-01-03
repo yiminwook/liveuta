df0be8ae0c9e4ea2d78168d7ed9dc62b690c0e6c
pekochan069 <pekochan069@gmail.com>
2025-01-04 02:58:08 +0900

    Move links to component for i18n

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -31,26 +31,6 @@ export const SETLIST_PAGE_SIZE = 15;
 
 export const ORIGIN = 'liveuta.vercel.app';
 
-export const INTERNAL_LINKS = [
-  { href: '/', text: '홈' },
-  { href: '/schedule', text: '스케줄' },
-  { href: '/multi', text: '멀티뷰' },
-  { href: '/channel', text: '채널' },
-  { href: '/setlist', text: '세트리' },
-  { href: '/setting', text: '설정' },
-  { href: '/dev', text: '개발' },
-];
-
-export const EXTERNAL_LINKS = [
-  {
-    href: 'https://gall.dcinside.com/mgallery/board/lists?id=kizunaai',
-    text: '키즈나아이 갤러리',
-  },
-  { href: 'https://gall.dcinside.com/mini/board/lists?id=vuta', text: '우타와꾸 갤러리' },
-  { href: 'https://uta-tools.vercel.app', text: '우타툴즈' },
-  { href: 'https://ezgif.com', text: 'EZ GIF' },
-];
-
 export const PORTAL_ID = 'liveuta-portal';
 
 export const SCHEDULE_CACHE_TIME = 3 * 60 * 1000;
