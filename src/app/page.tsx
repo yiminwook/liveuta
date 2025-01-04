c01380ce4041666e04e6af396791882deccc12cd
pekochan069 <pekochan069@gmail.com>
2025-01-05 00:51:23 +0900

    Redirect to locale home when reaching / route

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
 import { redirect } from '@/i18n/routing';
 import { siteConfig } from '@/siteConfig';
+import { cookies } from 'next/headers';
 
-export default function Page() {
-  redirect({ href: '/login', locale: siteConfig.defaultLocale });
+export default async function Page() {
+  const cookieStore = await cookies();
+  const locale = cookieStore.get('NEXT_LOCALE')?.value;
+
+  if (!locale) {
+    redirect({ href: '/', locale: siteConfig.defaultLocale });
+  }
+
+  redirect({ href: '/', locale: locale! });
 }
