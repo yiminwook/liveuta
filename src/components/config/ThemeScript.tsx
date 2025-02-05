7df3bf0b5d00fd18440055f67c4feade20494744
Minwook Yi <grs0416@gmail.com>
2025-02-06 00:42:11 +0900

    Update: theme script

--- src/components/config/ThemeScript.tsx
+++ src/components/config/ThemeScript.tsx
@@ -1,36 +1,42 @@
-/* eslint-disable @next/next/no-before-interactive-script-outside-document */
-import Script from 'next/script';
+// export const THEME_STORAGE_KEY = 'theme';
+// export const THEME_CUSTOM_EVENT_NAME = 'theme-change';
+// export const THEME_DARK_CLASS_NAME = 'dark';
+// export const THEME_LIGHT_CLASS_NAME = 'light';
 
-export const THEME_STORAGE_KEY = 'theme';
-export const THEME_CUSTOM_EVENT_NAME = 'theme-change';
-export const THEME_DARK_CLASS_NAME = 'dark';
-export const THEME_LIGHT_CLASS_NAME = 'light';
+// https://github.com/pacocoursey/next-themes/tree/main/next-themes/src
+
+function script() {
+  function isDarkModeEnabled(theme: string) {
+    const themeIndex = Number(theme.replace('theme', '')) || 1;
+    return themeIndex > 3;
+  }
+
+  function getThemePreference() {
+    if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+      return localStorage.getItem('theme') || 'theme1';
+    }
+    return 'theme1';
+  }
+
+  function setTheme(theme: string) {
+    const isDarkMode = isDarkModeEnabled(theme);
+    document.documentElement.setAttribute('color', theme);
+    document.documentElement.setAttribute(
+      'data-mantine-color-scheme',
+      isDarkMode ? 'dark' : 'light',
+    );
+  }
+
+  const initialTheme = getThemePreference();
+  console.log('initialTheme', initialTheme);
+  setTheme(initialTheme);
+}
 
 export default function ThemeScript() {
   return (
-    <Script id={THEME_CUSTOM_EVENT_NAME} strategy="beforeInteractive">{`
-
-      function isDarkModeEnabled (theme) {
-        const themeIndex = Number(theme.replace('theme', '')) || 1;
-        return themeIndex > 3;
-      };
-
-      function getThemePreference() {
-        if (typeof localStorage !== "undefined" && localStorage.getItem("${THEME_STORAGE_KEY}")) {
-          return localStorage.getItem("${THEME_STORAGE_KEY}") || "theme1";
-        }
-        return "theme1";
-      }
-
-      function setTheme(theme) {
-        const isDarkMode = isDarkModeEnabled(theme);
-        document.documentElement.setAttribute("color", theme);
-        document.documentElement.setAttribute("data-mantine-color-scheme", isDarkMode ? "dark" : "light");
-      }
-
-      const initialTheme = getThemePreference();
-      console.log("initialTheme", initialTheme);
-      setTheme(initialTheme);
-  `}</Script>
+    <script
+      // suppressHydrationWarning
+      dangerouslySetInnerHTML={{ __html: `(${script.toString()})()` }}
+    />
   );
 }
