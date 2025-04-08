351f17fa045cc0b2d7ade20a7bb361d1a95d0760
pekochan069 <pekochan069@gmail.com>
2025-04-09 03:53:39 +0900

    Update: Working base64 converter

---
+++ src/components/utils/converters/base64/Context.tsx
@@ -0,0 +1,93 @@
+import { Effect } from 'effect';
+import { useTranslations } from 'next-intl';
+import type { ReactNode } from 'react';
+import { createContext, memo, useContext, useMemo, useState } from 'react';
+import { execute } from './convert';
+
+type Base64ContextType = {
+  isEncode: boolean;
+  repeat: number;
+  input: string;
+  result: {
+    value: string;
+    error: boolean;
+  };
+};
+
+type Base64ActionsContextType = {
+  setIsEncode: (isEncode: boolean) => void;
+  setRepeat: (repeat: number) => void;
+  setInput: (input: string) => void;
+};
+
+const Base64Context = createContext<Base64ContextType | null>(null);
+const Base64ActionsContext = createContext<Base64ActionsContextType | null>(null);
+
+export function useBase64Context() {
+  const context = useContext(Base64Context);
+  if (context === null) {
+    throw new Error('useBase64Context must be used within a Base64Provider');
+  }
+  return context;
+}
+
+export function useBase64ActionsContext() {
+  const context = useContext(Base64ActionsContext);
+  if (context === null) {
+    throw new Error('useBase64ActionsContext must be used within a Base64Provider');
+  }
+  return context;
+}
+
+export function Base64Provider({ children }: { children: ReactNode }) {
+  const t = useTranslations('utils.converters.base64');
+  const [isEncode, setIsEncode] = useState(true);
+  const [repeat, setRepeat] = useState(1);
+  const [input, setInput] = useState('');
+
+  const result = useMemo(() => {
+    if (input === '') return { value: '', error: false };
+
+    const res = Effect.runSync(
+      execute(input, repeat, isEncode).pipe(
+        Effect.map((v) => ({
+          value: v.value,
+          error: false,
+        })),
+        Effect.catchAll((e) =>
+          Effect.succeed({
+            value: e.type === 'decode' ? t('decodeError') : t('encodeError'),
+            error: true,
+          }),
+        ),
+      ),
+    );
+
+    return res;
+  }, [input, repeat, isEncode]);
+
+  const value = useMemo(
+    () => ({
+      isEncode,
+      repeat,
+      input,
+      result,
+    }),
+    [isEncode, repeat, input, result],
+  );
+
+  const actions = useMemo(
+    () => ({
+      setIsEncode,
+      setRepeat,
+      setInput,
+    }),
+    [],
+  );
+
+  return (
+    <Base64ActionsContext.Provider value={actions}>
+      <Base64Context.Provider value={value}>{children}</Base64Context.Provider>
+    </Base64ActionsContext.Provider>
+  );
+}
--- src/components/utils/converters/base64/Home.tsx
+++ src/components/utils/converters/base64/Home.tsx
@@ -1,43 +1,47 @@
+'use client';
 import ClearButton from '@/components/common/button/ClearButton';
 import CopyButton from '@/components/common/button/CopyButton';
 import PasteButton from '@/components/common/button/PasteButton';
-import UtilsBreadcrumb from '@/components/utils/common/breadcrumb';
 import { MdiSwapVertical } from '@icons/mdi/swap-vertical';
 import { ActionIcon, NumberInput, Switch, Textarea, Tooltip } from '@mantine/core';
-import { Effect } from 'effect';
 import { useTranslations } from 'next-intl';
-import { ChangeEvent, useCallback, useMemo, useState } from 'react';
-import { execute } from './convert';
+import { ChangeEvent, useCallback, useEffect } from 'react';
+import { useUtilsBreadcrumbContext } from '../../common/BreadcrumbContext';
+import {
+  UtilsConfigItem,
+  UtilsConfigItemContents,
+  UtilsConfigItemHeader,
+  UtilsConfigRoot,
+} from '../../common/Config';
+import { Base64Provider, useBase64ActionsContext, useBase64Context } from './Context';
+import css from './Home.module.scss';
 
-export default function Base64Home() {
-  const t = useTranslations('utils.converters');
-  const [repeat, setRepeat] = useState(1);
-  const [isEncoding, setIsEncoding] = useState(true);
-  const [input, setInput] = useState('');
-  const result = useMemo(() => {
-    if (input === '') return { value: '', error: false };
-
-    const res = Effect.runSync(
-      execute(input, repeat, isEncoding).pipe(
-        Effect.map((v) => ({
-          value: v.value,
-          error: false,
-        })),
-        Effect.catchAll((e) =>
-          Effect.succeed({
-            value: e.type === 'decode' ? t('base64.decodeError') : t('base64.encodeError'),
-            error: true,
-          }),
-        ),
-      ),
-    );
-
-    return res;
-  }, [input, repeat, isEncoding]);
+function IsEncode() {
+  const t = useTranslations('utils.converters.base64');
+  const { setIsEncode } = useBase64ActionsContext();
+  const { isEncode } = useBase64Context();
 
   const handleIsEncodingChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setIsEncoding(e.currentTarget.checked);
+    setIsEncode(e.currentTarget.checked);
   }, []);
+
+  return (
+    <UtilsConfigItem>
+      <UtilsConfigItemHeader title={t('isEncodeTitle')} description={t('isEncodeDescription')} />
+      <UtilsConfigItemContents>
+        <label className={css.isEncodeLabel}>
+          <span className={css.isEncodeLabelText}>{isEncode ? t('encode') : t('decode')}</span>
+          <Switch checked={isEncode} onChange={handleIsEncodingChange} size="lg" />
+        </label>
+      </UtilsConfigItemContents>
+    </UtilsConfigItem>
+  );
+}
+
+function Repeat() {
+  const t = useTranslations('utils.converters.base64');
+  const { setRepeat } = useBase64ActionsContext();
+  const { repeat } = useBase64Context();
 
   const handleRepeatChange = useCallback((value: number | string) => {
     if (typeof value === 'number') {
@@ -46,12 +50,27 @@ export default function Base64Home() {
       setRepeat(Number.parseInt(value));
     }
   }, []);
+  return (
+    <UtilsConfigItem>
+      <UtilsConfigItemHeader title={t('repeatTitle')} description={t('repeatDescription')} />
+      <UtilsConfigItemContents>
+        <NumberInput
+          min={1}
+          value={repeat}
+          onChange={handleRepeatChange}
+          classNames={{
+            input: css.repeatInput,
+          }}
+        />
+      </UtilsConfigItemContents>
+    </UtilsConfigItem>
+  );
+}
 
-  const handleInputChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
-    setInput(e.currentTarget.value);
-  }, []);
-
-  const handleSwap = useCallback(() => setInput(result.value), [result]);
+function Base64Input() {
+  const t = useTranslations('utils.converters.base64');
+  const { setInput } = useBase64ActionsContext();
+  const { input } = useBase64Context();
 
   const paste = useCallback((value: ClipboardItem) => {
     const text = value.getType('text/plain');
@@ -60,54 +79,103 @@ export default function Base64Home() {
     }
   }, []);
 
+  const handleInputChange = useCallback((e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInput(e.currentTarget.value);
+  }, []);
+
   const clear = useCallback(() => setInput(''), []);
+  return (
+    <>
+      <div className={css.inputWrap}>
+        <label className={css.inputLabel} htmlFor="base64-input">
+          {t('input')}
+        </label>
+        <div>
+          <ClearButton clear={clear} />
+          <PasteButton paste={paste} />
+          <CopyButton value={input} />
+        </div>
+      </div>
+      <Textarea id="base64-input" value={input} onChange={handleInputChange} rows={8} />
+    </>
+  );
+}
+
+function Swap() {
+  const t = useTranslations('utils.converters.base64');
+  const { setInput } = useBase64ActionsContext();
+  const { result } = useBase64Context();
+
+  const handleSwap = useCallback(() => setInput(result.value), [result]);
 
   return (
-    <div>
-      <UtilsBreadcrumb
-        items={[
-          {
-            href: '/utils/converters',
-            title: t('title'),
-          },
-          {
-            href: '/utils/converters/base64',
-            title: t('base64.title'),
-          },
-        ]}
+    <Tooltip label={t('swap')} withArrow position="right">
+      <ActionIcon size={48} variant="outline" color="gray" onClick={handleSwap}>
+        <MdiSwapVertical width={32} height={32} />
+      </ActionIcon>
+    </Tooltip>
+  );
+}
+
+function Base64Output() {
+  const t = useTranslations('utils.converters.base64');
+  const { result } = useBase64Context();
+
+  return (
+    <>
+      <div className={css.inputWrap}>
+        <label className={css.inputLabel} htmlFor="base64-output">
+          {t('output')}
+        </label>
+        <div>
+          <CopyButton value={result.value} />
+        </div>
+      </div>
+      <Textarea
+        value={result.value}
+        readOnly
+        onClick={(e) => e.currentTarget.select()}
+        error={result.error}
+        rows={8}
       />
-      <div>
-        <Switch
-          checked={isEncoding}
-          onChange={handleIsEncodingChange}
-          label={isEncoding ? '인코딩' : '디코딩'}
-        />
-      </div>
-      <div>
-        <NumberInput min={1} value={repeat} onChange={handleRepeatChange} />
-      </div>
-      <div>
-        <ClearButton clear={clear} />
-        <PasteButton paste={paste} />
-        <CopyButton value={input} />
-        <Textarea value={input} onChange={handleInputChange} rows={8} />
-      </div>
-      <div>
-        <Tooltip label={t('base64.swap')} withArrow position="right">
-          <ActionIcon size={48} variant="outline" color="gray" onClick={handleSwap}>
-            <MdiSwapVertical width={32} height={32} />
-          </ActionIcon>
-        </Tooltip>
-      </div>
-      <div>
-        <Textarea
-          value={result.value}
-          readOnly
-          onClick={(e) => e.currentTarget.select()}
-          error={result.error}
-          rows={8}
-        />
-      </div>
-    </div>
+    </>
+  );
+}
+
+export default function Base64Home() {
+  const t = useTranslations('utils.converters');
+  const { setItems } = useUtilsBreadcrumbContext();
+
+  useEffect(() => {
+    setItems([
+      {
+        href: '/utils/converters',
+        title: t('title'),
+      },
+      {
+        href: '/utils/converters/base64',
+        title: t('base64.title'),
+      },
+    ]);
+  }, []);
+
+  return (
+    <Base64Provider>
+      <main className={css.wrap}>
+        <UtilsConfigRoot>
+          <IsEncode />
+          <Repeat />
+        </UtilsConfigRoot>
+        <div>
+          <Base64Input />
+        </div>
+        <div className={css.swapWrap}>
+          <Swap />
+        </div>
+        <div>
+          <Base64Output />
+        </div>
+      </main>
+    </Base64Provider>
   );
 }
