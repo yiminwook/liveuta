10182f1c2f595aadff1fc9b87e7d939c0882167b
pekochan069 <pekochan069@gmail.com>
2025-04-09 21:45:36 +0900

    Switch utils links back to regular internalLink

--- src/components/common/header/DesktopNav.tsx
+++ src/components/common/header/DesktopNav.tsx
@@ -16,15 +16,8 @@ export default function DesktopNav() {
     { href: '/featured', text: t('internalLinks.featured') },
     { href: '/multi', text: t('internalLinks.multiView') },
     { href: '/setlist', text: t('internalLinks.setlist') },
+    { href: '/utils', text: t('internalLinks.utils') },
     { href: '/dev', text: t('internalLinks.dev') },
-  ];
-
-  const utilsLinks = [
-    {
-      href: '/utils/converters',
-      text: t('utilsLinks.converter'),
-      subRoutes: [{ href: '/base64', text: t('utilsLinks.converters.base64') }],
-    },
   ];
 
   const externalLinks = [
@@ -43,9 +36,6 @@ export default function DesktopNav() {
     <ul>
       <li>
         <HeaderMenu title={t('internalLink')} links={internalLinks} onSelect={handleSelect} />
-      </li>
-      <li>
-        <HeaderMenu title={t('utilsLink')} links={utilsLinks} onSelect={handleSelect} />
       </li>
       <li>
         <HeaderMenu title={t('externalLink')} links={externalLinks} onSelect={handleSelect} />
--- src/components/common/header/HeaderMenu.tsx
+++ src/components/common/header/HeaderMenu.tsx
@@ -2,13 +2,11 @@ import { Menu, UnstyledButton } from '@mantine/core';
 import { useRouter } from 'next-nprogress-bar';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
-import For from '../utils/For';
-import Show from '../utils/Show';
 import css from './HeaderMenu.module.scss';
 
 type HeaderMenuProps = {
   title: string;
-  links: { href: string; text: string; subRoutes?: { href: string; text: string }[] }[];
+  links: { href: string; text: string }[];
   onSelect: (value: string) => void;
 };
 
@@ -23,48 +21,16 @@ export default function HeaderMenu({ title, links }: HeaderMenuProps) {
         <UnstyledButton className={css.trigger}>{title}</UnstyledButton>
       </Menu.Target>
       <Menu.Dropdown className={css.dropdown}>
-        {links.map((link) => {
-          if (link.subRoutes === undefined) {
-            return (
-              <Menu.Item
-                key={`headerMenu-${link.text}`}
-                data-current={pathname === link.href}
-                onClick={() => router.push(link.href)}
-                className={css.dropdownItem}
-              >
-                {link.text}
-              </Menu.Item>
-            );
-          } else {
-            return (
-              <Menu.Item
-                key={`headerMenu-${link.text}`}
-                data-current={pathname.startsWith(link.href)}
-                className={css.dropdownItem}
-              >
-                <Menu trigger="hover" offset={20} arrowOffset={15} withArrow position="right-start">
-                  <Menu.Target>
-                    <div>{link.text}</div>
-                  </Menu.Target>
-                  <Menu.Dropdown>
-                    <For each={link.subRoutes}>
-                      {(subRoute) => (
-                        <Menu.Item
-                          key={`headerMenu-${link.text}-${subRoute.text}`}
-                          data-current={pathname === `${link.href}${subRoute.href}`}
-                          onClick={() => router.push(`${link.href}${subRoute.href}`)}
-                          className={css.dropdownItem}
-                        >
-                          {subRoute.text}
-                        </Menu.Item>
-                      )}
-                    </For>
-                  </Menu.Dropdown>
-                </Menu>
-              </Menu.Item>
-            );
-          }
-        })}
+        {links.map((link) => (
+          <Menu.Item
+            key={`headerMenu-${link.text}`}
+            data-current={pathname === link.href}
+            onClick={() => router.push(link.href)}
+            className={css.dropdownItem}
+          >
+            {link.text}
+          </Menu.Item>
+        ))}
       </Menu.Dropdown>
     </Menu>
   );
