779e4bea751332057287cfdf242026fb70167047
Minwook Yi <grs0416@gmail.com>
2025-04-13 23:21:14 +0900

    Fix: GlobalCommands navigation url

--- src/components/common/command/GlobalCommands.tsx
+++ src/components/common/command/GlobalCommands.tsx
@@ -1,82 +1,80 @@
 'use client';
-
 import { useTranslations } from 'next-intl';
 import { useRouter } from 'next-nprogress-bar';
 import { useEffect } from 'react';
 import { useCommandActions } from './Context';
 
 export default function GlobalCommands() {
-  const t = useTranslations('global.command.globalCommands');
+  const t = useTranslations();
   const { addCmdGroup, setCmdOpen } = useCommandActions();
   const router = useRouter();
 
   useEffect(() => {
     addCmdGroup({
-      heading: t('navigate'),
+      heading: t('global.command.globalCommands.navigate'),
       commands: [
-        { title: t('navigation.home'), fn: () => router.push('/'), keywords: ['home'] },
         {
-          title: t('navigation.schedule'),
+          title: t('global.command.globalCommands.navigation.home'),
+          fn: () => router.push('/'),
+          keywords: ['home'],
+        },
+        {
+          title: t('global.command.globalCommands.navigation.schedule'),
           fn: () => router.push('/schedule'),
           keywords: ['navigate', 'schedules'],
         },
         {
-          title: t('navigation.multiView'),
-          fn: () => router.push('/multiView'),
+          title: t('global.command.globalCommands.navigation.multiView'),
+          fn: () => router.push('/multi'),
           keywords: ['navigate', 'multiView'],
         },
         {
-          title: t('navigation.channel'),
+          title: t('global.command.globalCommands.navigation.channel'),
           fn: () => router.push('/channel'),
           keywords: ['navigate', 'channel'],
         },
         {
-          title: t('navigation.setlist'),
+          title: t('global.command.globalCommands.navigation.setlist'),
           fn: () => router.push('/setlist'),
           keywords: ['navigate', 'setlist'],
         },
         {
-          title: t('navigation.settings'),
-          fn: () => router.push('/settings'),
-          keywords: ['navigate', 'settings'],
+          title: t('global.command.globalCommands.navigation.settings'),
+          fn: () => router.push('/setting'),
+          keywords: ['navigate', 'setting'],
         },
         {
-          title: t('navigation.dev'),
-          fn: () => router.push('/dev'),
-          keywords: ['navigate', 'dev'],
-        },
-        {
-          title: t('navigation.featured'),
+          title: t('global.command.globalCommands.navigation.featured'),
           fn: () => router.push('/featured'),
           keywords: ['navigate', 'featured'],
         },
         {
-          title: t('navigation.support'),
+          title: t('global.command.globalCommands.navigation.support'),
           fn: () => router.push('/support'),
           keywords: ['navigate', 'support'],
         },
       ],
     });
     addCmdGroup({
-      "heading": t('utility'),
+      heading: t('global.command.globalCommands.utility'),
       commands: [
         {
-          title: t('utilities.converters.base64'),
+          title: t('global.command.globalCommands.utilities.converters.base64'),
           fn: () => router.push('/utils/converters/base64'),
           keywords: ['utility', 'navigate', 'converters', 'base64'],
         },
         {
-          title: t('utilities.youtube.thumbnail'),
+          title: t('global.command.globalCommands.utilities.youtube.thumbnail'),
           fn: () => router.push('/utils/youtube/thumbnail'),
           keywords: ['utility', 'navigate', 'youtube', 'thumbnail'],
-        }
-      ]
+        },
+      ],
     });
     addCmdGroup({
-      heading: t('action'),
+      heading: t('global.command.globalCommands.action'),
       commands: [
         {
-          title: t('actions.commandPalette'),
+          title: t('global.command.globalCommands.actions.commandPalette'),
           fn: () => setCmdOpen(false),
           keywords: ['actions', 'commands', 'command palette'],
         },
